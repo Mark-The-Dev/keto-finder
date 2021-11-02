71165f1c74c7bed9043b8c41b9f0ab0dc7a51cf8
Mark <SwiftTactics@gmail.com>
2021-11-01 21:22:19 -0400

    initial commit

---
+++ src/main/java/com/example/ketofinder/KetoFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.ketofinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KetoFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KetoFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ketofinder/KetoFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ketofinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KetoFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
