debbe01547f5d45bd3daa78ec1fced345fcc36b3
Mark <SwiftTactics@gmail.com>
2021-11-07 18:21:50 -0500

    Added initial entities to project

---
+++ src/main/java/entity/RestaurantItemEntity.java
@@ -0,0 +1,43 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "restaurant_item")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestaurantItemEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    @Column(name="item_description")
+    private String itemDescription;
+
+    @Column(name="calories")
+    private float calories;
+
+    @Column(name="protein_value")
+    private float proteinValue;
+
+    @Column(name="fat_value")
+    private float fatValue;
+
+    @Column(name="carb_value")
+    private float carbValue;
+
+    @Column(name="net_carb_value")
+    private float netCarbValue;
+
+    @ManyToOne
+    @JoinColumn(name="restaurant_id")
+    private RestaurantListEntity restaurantListEntity;
+}
---
+++ src/main/java/entity/RestaurantListEntity.java
@@ -0,0 +1,21 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "restaurant_list")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestaurantListEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "restaurant_name")
+    private String restaurantName;
+}
