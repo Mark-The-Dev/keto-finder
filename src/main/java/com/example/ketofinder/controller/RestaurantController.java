4e4c310925271617ef9e1be0ec3df457cc0a9c80
Mark <SwiftTactics@gmail.com>
2021-11-08 23:06:41 -0500

    added cross origin for cors

--- src/main/java/com/example/ketofinder/controller/RestaurantController.java
+++ src/main/java/com/example/ketofinder/controller/RestaurantController.java
@@ -3,6 +3,7 @@ package com.example.ketofinder.controller;
 import com.example.ketofinder.dto.RestaurantBundleDTO;
 import com.example.ketofinder.service.RestaurantListService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class RestaurantController {
     }
 
     @GetMapping("/restaurants")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<RestaurantBundleDTO> findAllRestaurants() {
         return restaurantListService.findAllItems();
     }
